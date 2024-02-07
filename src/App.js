6d708c5b6cc295aa106532a54aea27fc8047a570
Susan Zhou <susan.0.zhou@gmail.com>
2024-02-06 22:04:14 -0800

    starting to implement the routing functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { getAllAnimes }  from './assets/AnimeService';  
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
@@ -9,7 +11,6 @@ function App() {
   const getAnimes = async() => {
     try {
       const response = await getAllAnimes();
-      console.log(response.data);
       setAnimes(response.data);
     } catch(err) {
       console.error(err);
@@ -22,7 +23,9 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={Layout}></Route>
+      </Routes>
     </div>
   );
 }
