96f79533e576db7ef1e5ab07dd6182e97a438914
Susan Zhou <susan.0.zhou@gmail.com>
2024-02-08 23:37:20 -0800

    adding anime reviewForm and review component

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,51 @@
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
+
+const Reviews = ({reviews, setReviews}) => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h3>Reviews</h3>
+                </Col>
+            </Row>
+            <Row className="mt-2">
+                <Col>
+                    <img src={anime?.poster} alt="" />
+                </Col>
+                <Col>
+                    {
+                        <>
+                            <Row>
+                                <Col>
+                                    <ReviewForm handleSubmit={addReview} revText={revText} labelText={"Write a review"}/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <hr />
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                    {
+                        reviews?.map((review) => {
+                            <>
+                                <Row>
+                                    <Col>{review.body}</Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <hr />
+                                    </Col>
+                                </Row>
+                            </>
+                        })
+                    }
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Reviews;
