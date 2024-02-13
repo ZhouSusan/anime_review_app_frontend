fab98845fe50df14a25d15eb23d4a5f6814db489
Susan Zhou <susan.0.zhou@gmail.com>
2024-02-12 22:33:46 -0800

    editing base url and creating a new instance of axios with a custom config

--- src/assets/AnimeService.js
+++ src/assets/AnimeService.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_REST_API_URL = 'http://localhost:8080/api/v1/animes';
-
-export const getAllAnimes = () => axios.get(BASE_REST_API_URL);
+export default axios.create({
+    baseURL:'http://localhost:8080/api/v1',
+});
